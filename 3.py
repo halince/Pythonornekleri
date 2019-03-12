d54098cb78f0768df5aaa4f9f975c959a22b16c2
halince <48432302+halince@users.noreply.github.com>
2019-03-12 14:03:46 +0300

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,3 @@
+sayi1=int(input("Birnci sayı girinizi"))
+sayi2=int(input("Bir sayı girinizi"))
+print("girdiğiniz sayıların \n toplamı=",sayi1+sayi2,"\n","farkı=",sayi1-sayi2,"\n","çarpımı=",sayi1*sayi2,"\n","bölümü=",sayi1/sayi2)
